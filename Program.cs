b67383703bb3ac012eadb2efdf6079f113aa0fbb
Влад Кобзар <vlad.kobzar14@gmail.com>
2023-09-20 13:06:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Student;
+
+class Program
+{
+    static void Main()
+    {
+        int[] homeworks = new[] { 66, 77, 87, 100, 50, 66, 78 };
+        int[] finWorks = new[] { 78, 75, 82, 100, 95, 65 };
+        int[] exams = new[] { 90, 71, 87, 100 };
+        Student.Student student1 = new Student.Student("Smith", "John", "Joich", new DateTime(2006, 3, 15), new Address("123 Main St", "London", "UK"), "123-456-7890", homeworks, finWorks, exams);
+        Student.Student student2 = new Student.Student("Anderson", "Tom", "Markovich", new DateTime(1990, 12, 21), new Address("12 Vilcov St", "Odesa", "UKR"), "321-426-7890", new int[] { 100,90,88,75}, new int[] { 72, 90 }, new int[] { 100, 90 });
+        Student.Student student3 = new Student.Student("Paterson", "Mark", "Leonovich");
+        Student.Student student4 = new Student.Student("Alvares", "Juan", new int[] { 98,77,65,89,90,66}, new int[] { 59,61,99,88}, new int[] { 90,76,88});
+        
+        student2.AddHomeworkGrade(95);
+        student2.AddFinalPaperGrade(88);
+        student2.AddExamGrade(75);
+
+        student3.AddHomeworkGrade(77);
+        student3.AddHomeworkGrade(95);
+        student3.AddHomeworkGrade(83);
+        student3.AddFinalPaperGrade(69);
+        student3.AddFinalPaperGrade(74);
+        student3.AddExamGrade(90);
+        student3.AddExamGrade(78);
+        student3.PhoneNumber = "432-567-8788";
+
+        student4.HomeAddress = new Address("luice St","Mexico-City","Mexica");
+
+        student1.ShowStudentInfo();
+        student2.ShowStudentInfo();
+        student3.ShowStudentInfo();
+        student4.ShowStudentInfo();
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,140 @@
+﻿namespace Student
+{
+    public class Student
+    {
+        // Поля
+        private string lastName;
+        private string firstName;
+        private string patronymic;
+        private DateTime birthdate;
+        private Address address;
+        private string phoneNumber;
+        private int[] homeworks;
+        private int[] finalWorks;
+        private int[] exams;
+
+        // Конструктори
+        public Student(string lastName, string firstName, string patronymic,
+            DateTime birthdate, Address address, string phoneNumber,
+            int[] homework, int[] finalWork, int[] exams)
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.patronymic = patronymic;
+            this.birthdate = birthdate;
+            this.address = address;
+            this.phoneNumber = phoneNumber;
+            this.homeworks = homework;
+            this.finalWorks = finalWork;
+            this.exams = exams;
+        }
+
+        public Student(string lastName, string firstName, string patronymic, DateTime birthdate,
+            Address homeAddress, string phoneNumber)
+            : this(lastName, firstName, patronymic, birthdate, homeAddress,
+                  phoneNumber, new int[0], new int[0], new int[0])
+        {
+        }
+
+        public Student(string lastName, string firstName, string patronymic)
+            : this(lastName, firstName, patronymic, new DateTime(2000, 1, 1),
+                  new Address("", "", ""), string.Empty, new int[0], new int[0], new int[0])
+        {
+        }
+
+        public Student(string lastName, string firstName, int[] homework, int[] finalWork, int[] exams)
+            : this(lastName, firstName, string.Empty, new DateTime(2000,1,1),
+                  new Address("", "", ""), string.Empty, homework, finalWork, exams)
+        {
+        }
+
+        // Властивості
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string Patronymic
+        {
+            get { return patronymic; }
+            set { patronymic = value; }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return birthdate; }
+            set { birthdate = value; }
+        }
+
+        public Address HomeAddress
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        // Методи
+        public void AddHomeworkGrade(int grade)
+        {
+            Array.Resize(ref homeworks, homeworks.Length + 1);  // ref вказує на те, що ми змінюємо сам масив, а не створюємо новий
+            homeworks[homeworks.Length - 1] = grade;
+        }
+
+        public void AddFinalPaperGrade(int grade)
+        {
+            Array.Resize(ref finalWorks, finalWorks.Length + 1);
+            finalWorks[finalWorks.Length - 1] = grade;
+        }
+
+        public void AddExamGrade(int grade)
+        {
+            Array.Resize(ref exams, exams.Length + 1);
+            exams[exams.Length - 1] = grade;
+        }
+
+        public void ShowStudentInfo()
+        {
+            Console.WriteLine("\tІнформація про студента:");
+            Console.WriteLine($"ФІО: {firstName} {lastName} {patronymic}");
+            Console.WriteLine($"Дата народження: {birthdate.ToShortDateString()}");
+            Console.WriteLine($"Адреса: {address}");
+            Console.WriteLine($"Номер телефону: {phoneNumber}");
+            Console.WriteLine($"Оцінки за домашні роботи: {string.Join(", ", homeworks)}");
+            Console.WriteLine($"Оцінки за підсумкові роботи: {string.Join(", ", finalWorks)}");
+            Console.WriteLine($"Оцінки за екзамени: {string.Join(", ", exams)}\n\n");
+        }
+    }
+
+    // Окремий клас для адреси
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public Address(string street, string city, string country)
+        {
+            Street = street;
+            City = city;
+            Country = country;
+        }
+
+        public override string ToString()
+        {
+            return $"{Street}, {City}, {Country}";
+        }
+
+    }
+}
